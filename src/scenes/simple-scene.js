80b2ebccf7cb9a42b742913c975c6f4d2b1e040a
Bill Reed <billnreed@gmail.com>
2018-12-25 19:36:58 -0500

    shoot arrow from anchor point towards mouse with angle

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -7,27 +7,36 @@ export class GameScene extends Phaser.Scene {
   create() {
     this.input.setDefaultCursor('none');
 
-    this.arrow = this.physics.add.image(50, 240, 'arrow');
+    this.arrow = this.physics.add.image(50, 400, 'arrow');
     this.arrow.body.allowGravity = false;
 
-    this.target = this.physics.add.image(600, 240, 'target');
+    this.target = this.physics.add.image(600, 400, 'target');
     this.target.body.allowGravity = false;
     this.target.body.immovable = true;
 
-    this.input.on('pointermove', this._moveArrowWithMouse, this);
+    this.input.on('pointermove', this._angleArrowWithMouse, this);
 
-    this.input.on('pointerdown', pointer => {
+    this.input.once('pointerdown', pointer => {
+      this.input.off('pointermove', this._angleArrowWithMouse, this);
+
       this.arrow.body.allowGravity = true;
-      this.arrow.body.velocity.x = 600;
+      this.physics.velocityFromRotation(this.arrow.rotation, 500, this.arrow.body.velocity)
     });
 
     this.physics.add.collider(this.arrow, this.target, (arrow, target) => {
+      arrow.body.allowGravity = false;
       arrow.body.enable = false;
-      this.input.off('pointermove', this._moveArrowWithMouse, this);
     });
   }
 
-  _moveArrowWithMouse(pointer) {
-    this.arrow.y = pointer.y;
+  _angleArrowWithMouse(pointer) {
+    const angle = Phaser.Math.Angle.BetweenPoints(this.arrow, pointer);
+    this.arrow.rotation = angle;
+  }
+
+  update() {
+    if (this.arrow.body.allowGravity) {
+      this.arrow.rotation = Phaser.Math.Angle.BetweenPoints(Phaser.Math.Vector2.ZERO, this.arrow.body.velocity);
+    }
   }
 }
