ed9106ddb8c4c8a676f4883640e87febca4871ac
Bill Reed <billnreed@gmail.com>
2018-12-25 20:02:04 -0500

    reset arrow and target after a hit

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -5,14 +5,70 @@ export class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.input.setDefaultCursor('none');
+    this.input.setDefaultCursor('crosshair');
 
     this.arrow = this.physics.add.image(50, 400, 'arrow');
+    this.arrow.setScale(0.75);
     this.arrow.body.allowGravity = false;
 
     this.target = this.physics.add.image(600, 400, 'target');
     this.target.body.allowGravity = false;
     this.target.body.immovable = true;
+
+    this._resetArrow();
+    this._setTargetToRandomPosition();
+
+    this.physics.add.collider(this.arrow, this.target, (arrow, target) => {
+      arrow.body.allowGravity = false;
+      arrow.body.enable = false;
+
+      this.tweens.add({
+        targets: [this.arrow, this.target],
+        props: {
+          alpha: 0,
+        },
+        duration: 200,
+        yoyo: true,
+        repeat: 1,
+        onComplete: () => {
+          this.tweens.add({
+            targets: [this.arrow, this.target],
+            props: {
+              alpha: 0,
+            },
+            duration: 200,
+            onComplete: () => {
+              this._resetArrow();
+              this._setTargetToRandomPosition();
+            },
+          })
+        },
+      });
+    });
+  }
+
+  update() {
+    if (this.arrow.body.allowGravity) {
+      this.arrow.rotation = Phaser.Math.Angle.BetweenPoints(Phaser.Math.Vector2.ZERO, this.arrow.body.velocity);
+    }
+  }
+
+  _angleArrowWithMouse(pointer) {
+    const angle = Phaser.Math.Angle.BetweenPoints(this.arrow, pointer);
+    this.arrow.rotation = angle;
+  }
+
+  _resetArrow() {
+    this.arrow.x = 50;
+    this.arrow.y = 400;
+
+    this.arrow.alpha = 1;
+
+    this.arrow.body.enable = true;
+    this.arrow.body.velocity.x = 0;
+    this.arrow.body.velocity.y = 0;
+
+    this._angleArrowWithMouse(this.input.mousePointer);
 
     this.input.on('pointermove', this._angleArrowWithMouse, this);
 
@@ -22,21 +78,12 @@ export class GameScene extends Phaser.Scene {
       this.arrow.body.allowGravity = true;
       this.physics.velocityFromRotation(this.arrow.rotation, 500, this.arrow.body.velocity)
     });
-
-    this.physics.add.collider(this.arrow, this.target, (arrow, target) => {
-      arrow.body.allowGravity = false;
-      arrow.body.enable = false;
-    });
   }
 
-  _angleArrowWithMouse(pointer) {
-    const angle = Phaser.Math.Angle.BetweenPoints(this.arrow, pointer);
-    this.arrow.rotation = angle;
-  }
+  _setTargetToRandomPosition() {
+    this.target.x = Phaser.Math.RND.integerInRange(500, 650);
+    this.target.y = Phaser.Math.RND.integerInRange(300, 500);
 
-  update() {
-    if (this.arrow.body.allowGravity) {
-      this.arrow.rotation = Phaser.Math.Angle.BetweenPoints(Phaser.Math.Vector2.ZERO, this.arrow.body.velocity);
-    }
+    this.target.alpha = 1;
   }
 }
