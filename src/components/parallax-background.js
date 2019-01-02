52e531702d923a17816181afba79956735d7a0bc
Bill Reed <billnreed@gmail.com>
2019-01-01 21:23:43 -0500

    using scroll factor for parallax tilesprites

--- src/components/parallax-background.js
+++ src/components/parallax-background.js
@@ -7,24 +7,22 @@ export class ParallaxBackground {
     this.backgroundBack = scene.add.tileSprite(0, 0, 640, 300, backKey);
     this.backgroundBack.setOrigin(0, 0);
     this.backgroundBack.setTileScale(1, 1.35);
+    this.backgroundBack.setScrollFactor(0);
 
     this.backgroundMiddle = scene.add.tileSprite(0, 0, 640, 300, middleKey);
     this.backgroundMiddle.setOrigin(0, 0);
     this.backgroundMiddle.setTileScale(1, 1.35);
+    this.backgroundMiddle.setScrollFactor(0);
 
     this.backgroundFront = scene.add.tileSprite(0, 0, 640, 300, frontKey);
     this.backgroundFront.setOrigin(0, 0);
     this.backgroundFront.setTileScale(1, 1.35);
+    this.backgroundFront.setScrollFactor(0);
   }
 
   update(scrollAmount) {
-    this.backgroundBack.x = scrollAmount;
     this.backgroundBack.tilePositionX = scrollAmount / 3;
-
-    this.backgroundMiddle.x = scrollAmount;
     this.backgroundMiddle.tilePositionX = scrollAmount / 2;
-
-    this.backgroundFront.x = scrollAmount;
     this.backgroundFront.tilePositionX = scrollAmount;
   }
 
@@ -32,7 +30,6 @@ export class ParallaxBackground {
     this.scene.tweens.add({
       targets: [this.backgroundBack, this.backgroundMiddle, this.backgroundFront],
       props: {
-        x: 0,
         tilePositionX: 0,
       },
       duration: 300,
