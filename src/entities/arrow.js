87d8298ab64de9a70277c778baf7a4e242c3bff9
Bill Reed <billnreed@gmail.com>
2019-03-17 22:52:24 -0400

    sync hitbox with arrow every update

--- src/entities/arrow.js
+++ src/entities/arrow.js
@@ -8,7 +8,10 @@ const arrowConfig = config.entities.game.arrow;
 export class Arrow {
   constructor(scene) {
     this.scene = scene;
+
     this.sprite = this.scene.physics.add.sprite(0, 0, 'arrow');
+    this.sprite.setDisplaySize(48, 12);
+
     this.hitbox = this.scene.add.zone(0, 0, 12, 12);
     this.scene.physics.add.existing(this.hitbox);
 
@@ -18,10 +21,6 @@ export class Arrow {
       high: this.scene.sound.add('arrow-release-high'),
     };
 
-    this.sprite.setDisplaySize(48, 12);
-    this.sprite.body.allowGravity = false;
-    this.hitbox.body.allowGravity = false;
-
     this.reset();
   }
 
@@ -30,10 +29,12 @@ export class Arrow {
 
     if (state === STATES.FLY) {
       this.sprite.rotation = Phaser.Math.Angle.BetweenPoints(Phaser.Math.Vector2.ZERO, this.sprite.body.velocity);
+      this.syncHitbox();
     }
 
     if (state === STATES.REST || state === STATES.CHARGE) {
       this.angleToPointer();
+      this.syncHitbox();
     }
 
     if (state === STATES.CHARGE) {
@@ -48,16 +49,20 @@ export class Arrow {
   }
 
   getHitbox() {
-    return this.sprite;
+    return this.hitbox;
   }
 
   angleToPointer() {
-    const angleInRadians = Phaser.Math.Angle.BetweenPoints(this.sprite, this.scene.input.activePointer);
-    this.sprite.rotation = angleInRadians;
+    const angle = Phaser.Math.Angle.BetweenPoints(this.sprite, this.scene.input.activePointer);
+    this.sprite.rotation = angle;
+  }
 
+  syncHitbox() {
+    const angle = this.sprite.rotation;
     const magnitude = 24;
-    const x = Math.cos(angleInRadians) * magnitude;
-    const y = Math.sin(angleInRadians) * magnitude;
+
+    const x = Math.cos(angle) * magnitude;
+    const y = Math.sin(angle) * magnitude;
 
     this.hitbox.x = this.sprite.x + x;
     this.hitbox.y = this.sprite.y + y;
@@ -73,34 +78,29 @@ export class Arrow {
       this.releaseSounds.high.play();
     }
 
+    this.hitbox.body.enable = true;
     this.sprite.body.allowGravity = true;
-    this.hitbox.body.allowGravity = true;
     this.scene.physics.velocityFromRotation(this.sprite.rotation, this.scene.registry.get('charge'), this.sprite.body.velocity)
-    this.scene.physics.velocityFromRotation(this.sprite.rotation, this.scene.registry.get('charge'), this.hitbox.body.velocity)
   }
 
   reset() {
     this.scene.registry.set('charge', arrowConfig.minCharge);
 
     this.sprite.body.enable = true;
-    this.hitbox.body.enable = true;
+    this.hitbox.body.enable = false;
 
     this.sprite.x = arrowLayoutConfig.x;
     this.sprite.y = arrowLayoutConfig.y;
-    this.hitbox.x = arrowLayoutConfig.x + 18;
-    this.hitbox.y = arrowLayoutConfig.y;
 
     this.sprite.alpha = 1;
-    this.hitbox.alpha = 1;
 
-    this.sprite.body.enable = true;
     this.sprite.body.allowGravity = false;
     this.sprite.body.velocity.x = 0;
     this.sprite.body.velocity.y = 0;
-    this.hitbox.body.enable = true;
+
     this.hitbox.body.allowGravity = false;
-    this.hitbox.body.velocity.x = 0;
-    this.hitbox.body.velocity.y = 0;
+
+    this.syncHitbox();
   }
 
   onHit() {
