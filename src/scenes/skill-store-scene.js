610176c264fce724a3fe9298ed13e4fbd665d103
Bill Reed <billnreed@gmail.com>
2019-05-11 14:16:40 -0400

    skill buttons with descriptions on mouseover

--- src/scenes/skill-store-scene.js
+++ src/scenes/skill-store-scene.js
@@ -1,4 +1,7 @@
 import { config } from '../config';
+import { SpectralArrowIcon } from '../ui/spectral-arrow-icon';
+import { SplitArrowIcon } from '../ui/split-arrow-icon';
+import { CannonballIcon } from '../ui/cannonball-icon';
 
 export class SkillStoreScene extends Phaser.Scene {
   constructor() {
@@ -19,11 +22,104 @@ export class SkillStoreScene extends Phaser.Scene {
 
     this.add.bitmapText(320, 50, 'font-outline', 'Skill Store', 12)
       .setOrigin(0.5);
+
+    this._createSpectralArrowButton();
+    this._createSplitArrowButton();
+    this._createCannonballButton();
+
+    this._createDescriptionBackground();
+    this._createDescriptionTexts();
   }
 
   _createBackgroundImage(key) {
     this.add.image(0, 0, key)
       .setDisplaySize(config.dimensions.viewport.width, config.dimensions.viewport.height)
       .setOrigin(0);
+  }
+
+  _createSpectralArrowButton() {
+    const x = 130;
+    const y = 120;
+
+    this.add.image(x, y, 'skill-background')
+      .setDisplaySize(42, 42)
+      .setInteractive({ cursor: 'pointer' })
+      .on('pointerover', () => this._displayDescriptionFor('spectralArrow'))
+      .on('pointerout', () => this._hideDescription());
+    new SpectralArrowIcon(this, x, y);
+
+    const costText = this.add.bitmapText(x - 5, y + 40, 'font', '25', 18)
+      .setOrigin(0.5);
+     this.add.image(x + costText.width - 5, y + 40, 'gold-3');
+
+    this.add.bitmapText(x, y + 65, 'font', `Charges: `, 18)
+      .setOrigin(0.5);
+  }
+
+  _createSplitArrowButton() {
+    const x = 310;
+    const y = 120;
+
+    this.add.image(x, y, 'skill-background')
+      .setDisplaySize(42, 42)
+      .setInteractive({ cursor: 'pointer' })
+      .on('pointerover', () => this._displayDescriptionFor('splitArrow'))
+      .on('pointerout', () => this._hideDescription());
+
+    new SplitArrowIcon(this, x, y);
+  }
+
+  _createCannonballButton() {
+    const x = 490;
+    const y = 120;
+
+    this.add.image(x, y, 'skill-background')
+      .setDisplaySize(42, 42)
+      .setInteractive({ cursor: 'pointer' })
+      .on('pointerover', () => this._displayDescriptionFor('cannonball'))
+      .on('pointerout', () => this._hideDescription());
+
+    new CannonballIcon(this, x, y);
+  }
+
+  _createDescriptionBackground() {
+    this.descriptionBackground = this.add.graphics();
+    this.descriptionBackground.fillStyle(0x222222, 0.7);
+    this.descriptionBackground.fillRect(50, 200, 540, 80);
+    this.descriptionBackground.alpha = 0;
+  }
+
+  _createDescriptionTexts() {
+    const skillDescriptions = {
+      'spectralArrow': 'Your arrow becomes spectral and can pass through targets.',
+      'cannonball': "Drop a cannonball from your arrow as it flies. Don't ask how.",
+      'splitArrow': 'Your single arrow splits into 3 new arrows.',
+    };
+
+    const spectralArrowText = this.add.bitmapText(320, 240, 'font', skillDescriptions['spectralArrow'], 18)
+      .setOrigin(0.5)
+      .setAlpha(0);
+    const splitArrowText = this.add.bitmapText(320, 240, 'font', skillDescriptions['splitArrow'], 18)
+      .setOrigin(0.5)
+      .setAlpha(0);
+    const cannonballText = this.add.bitmapText(320, 240, 'font', skillDescriptions['cannonball'], 18)
+      .setOrigin(0.5)
+      .setAlpha(0);
+
+    this.skillTexts = {
+      'spectralArrow': spectralArrowText,
+      'splitArrow': splitArrowText,
+      'cannonball': cannonballText,
+    }
+  }
+
+  _displayDescriptionFor(skill) {
+    this.descriptionBackground.alpha = 1;
+    this.skillTexts[skill].alpha = 1;
+  }
+
+  _hideDescription() {
+    this.descriptionBackground.alpha = 0;
+    Object.values(this.skillTexts).forEach(text => text.alpha = 0);
   }
 }
